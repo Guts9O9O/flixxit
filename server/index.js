d4bdf2f740f86f4b5ee536d2d7f528bd64160787
Guts9O9O <chinmays2001@gmail.com>
2024-12-10 19:18:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,26 +11,9 @@ const app = express();
 app.use(
   cors({
     origin: ["https://flixxit-umber.vercel.app", "http://localhost:3000"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow all required methods
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "x-csrf-token", // Add any custom headers your client uses
-    ],
     credentials: true,
   })
 );
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://flixxit-frontend.vercel.app"); // Allow your frontend origin
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Authorization, Content-Type");
-  res.header("Access-Control-Allow-Credentials", "true"); // Required for cookies or credentials
-  if (req.method === "OPTIONS") {
-    return res.status(200).end(); // Handle preflight requests
-  }
-  next();
-});
 
 app.options("*", cors());
 app.use(express.json());
