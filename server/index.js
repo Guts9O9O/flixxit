9653750e59ffcc9b1666b7088cd18113dce2c589
Guts9O9O <chinmays2001@gmail.com>
2024-12-15 16:04:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://flixxit-umber.vercel.app", "flixxit-backend-7w6k7mz5i-chinmays-projects-972d344e.vercel.app"],
+    origin: ["https://flixxit-delta.vercel.app", "flixxit-backend-7w6k7mz5i-chinmays-projects-972d344e.vercel.app"],
     credentials: true,
   })
 );
